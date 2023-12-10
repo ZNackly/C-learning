fed117a0f1ee256defb4c1c57183b306f6d25fdc
Dan <137257813+ZNackly@users.noreply.github.com>
2023-12-10 11:42:52 +0500

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,8 @@
+cout << "_____________" << endl;
+cout << "|  /\"\"\"\"\"\\  |" << endl;
+cout << "| /       \\ |" << endl;
+cout << "| | 0   0 | |" << endl;
+cout << "| |   v   | |" << endl;
+cout << "| \\  ___  / |" << endl;
+cout << "|  \\_____/  |" << endl;
+cout << "-------------" << endl;
